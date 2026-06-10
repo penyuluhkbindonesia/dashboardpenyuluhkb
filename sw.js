8d53a392841f95c827e25f9e7089817178d833f1
PenyuluhKB <penyuluhkb.indonesia@gmail.com>
2026-06-10 16:08:10 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pkb-pwa-v1';
+const CACHE_NAME = 'pkb-pwa-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -6,22 +6,14 @@ const urlsToCache = [
   './manifest.json'
 ];
 
-// Menginstal Service Worker dan menyimpan cache
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Mengambil data dari cache jika offline
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
