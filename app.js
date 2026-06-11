24cab90160314f0237a91ad64e591a1c2a178fd4
PenyuluhKB <penyuluhkb.indonesia@gmail.com>
2026-06-11 09:19:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const SUPABASE_URL = 'https://cdnqqrjbdhoglvlqbxoq.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkbnFxcmpiZGhvZ2x2bHFieG9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMDQ1NDIsImV4cCI6MjA5NjU4MDU0Mn0.dHQbkEIJe5L4bfyJqZkJkXTPX0Abot4GBw7_4O3eNwk';
 const mySupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-const SUPABASE_URL = 'https://GANTI_DENGAN_PROJECT_URL_ANDA.supabase.co';
-const SUPABASE_ANON_KEY = 'GANTI_DENGAN_ANON_KEY_ANDA';
-const mySupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 let filterProvinsiAktif = null;
 let currentZoomLevel = 100; 
 
